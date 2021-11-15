a4bb36e04f79c0445fe8b81d5af416d0865ddc05
bertagrim <bertagrim@gmail.com>
2021-11-15 21:54:03 +0100

    fixed all bugs (for now)

--- src/config.py
+++ src/config.py
@@ -5,5 +5,26 @@ body_limits = {
     '2015_Book_LinearAlgebra': [12, 311],
     '2017_Book_CosmologyForTheCurious': [12, 353],
     '2017_Book_RoboticsVisionAndControl': [25, 651],
-    '2017_Book_TheDataScienceDesignManual': [18, 424]
+    '2017_Book_TheDataScienceDesignManual': [18, 424],
+    'christensen_04': [13,190],
+    'heather_17': [17,216],
+    'jacobson_14': [23,426],
+    'jenkins_08': [15,285],
+    'keefe_00': [18,232],
+    'linnebo_18': [11, 240],
+    'linnebo_21': [11, 310],
+    'lycan_08': [13,213],
+    'lyons_96': [18,360],
+    'moltmann_97': [15,258],
+    'morris_07': [12,326],
+    'nussbaumer_15': [16,270],
+    'oliver_13': [15,314],
+    'peregrin_17': [9,164],
+    'shapiro_97': [14,272],
+    'stenning_08': [15,380],
+    'williamson_94': [15,320],
+    'winter_16': [13,257],
+    'zimmermann_13': [11,271]
+    #TODO: repassar
+    
 }
