291e554448e4d4b179730b7ba5db3fddc8948ec1
bertagrim <bertagrim@gmail.com>
2021-11-15 22:50:25 +0100

    complete config file

--- src/config.py
+++ src/config.py
@@ -1,30 +1,30 @@
+
 # file_base_name => [from, to]
 body_limits = {
-    'cann_93': [19, 340],
-    'winter_16': [13, 257],
     '2015_Book_LinearAlgebra': [12, 311],
     '2017_Book_CosmologyForTheCurious': [12, 353],
     '2017_Book_RoboticsVisionAndControl': [25, 651],
-    '2017_Book_TheDataScienceDesignManual': [18, 424],
+    '2017_Book_TheDataScienceDesignManual': [18, 437],
     'christensen_04': [13,190],
     'heather_17': [17,216],
-    'jacobson_14': [23,426],
+    'jacobson_14': [21,426],
     'jenkins_08': [15,285],
-    'keefe_00': [18,232],
+    'keefe_00': [13,232],
     'linnebo_18': [11, 240],
-    'linnebo_21': [11, 310],
-    'lycan_08': [13,213],
-    'lyons_96': [18,360],
+    'linnebo_21': [15, 310],
+    'lycan_08': [13,212],
+    'lyons_96': [18,359],
     'moltmann_97': [15,258],
     'morris_07': [12,326],
     'nussbaumer_15': [16,270],
     'oliver_13': [15,314],
-    'peregrin_17': [9,164],
+    'peregrin_17': [9,173],
     'shapiro_97': [14,272],
     'stenning_08': [15,380],
     'williamson_94': [15,320],
     'winter_16': [13,257],
-    'zimmermann_13': [11,271]
-    #TODO: repassar
-    
+    'zimmermann_13': [11,271],
+    '2007BoolosComputability and Logic Fifth Edition': [18, 355],
+    '2009_Book_TheElementsOfStatisticalLearni': [21,710],
+    '2014_Book_Microeconomics': [12,510] 
 }
